1e33b61d83ca3773c22fa4666e2a69af97190d4f
chumoath <chumoath.guo@gmail.com>
2024-04-28 21:41:37 +0800

    修改avr启动脚本

--- avr/start_boot.sh
+++ avr/start_boot.sh
@@ -7,9 +7,12 @@ gdb -args ../../qemu-8.1.0/build/qemu-system-avr \
 	-bios ./firmware/qemu-avr-tests/free-rtos/Demo/AVR_ATMega2560_GCC/demo.elf \
 	-s \
 	-serial tcp::5678,server=on,wait=off \
+	-D log.txt \
+	-monitor pipe:mon \
+	-d in_asm,op,out_asm \
+	#-plugin ../../qemu-8.1.0/build/contrib/plugins/libexeclog.so \
+	#-d plugin \
 
-	#-D log.txt \
-	#-d in_asm
 
 	#-s -S
 	#-bios ./firmware/blinky.elf \
