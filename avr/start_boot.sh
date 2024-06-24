9bc6269d1aa232cfe5d3cd2bb5efe928a1ab3903
chumoath <chumoath.guo@gmail.com>
2024-06-24 22:46:27 +0800

    添加lc3 run

--- avr/start_boot.sh
+++ avr/start_boot.sh
@@ -3,15 +3,17 @@
 
 #gdb -args ../../qemu-8.1.0/build/qemu-system-avr \
 #../../qemu-8.1.0/build/qemu-system-avr \
-../../../../qemu-lc3/qemu-8.1.0/build/qemu-system-lc3 \
+#../../../../qemu-lc3/qemu-8.1.0/build/qemu-system-lc3 \
+gdb -args ../../../../qemu-lc3/qemu-8.1.0/build/qemu-system-lc3 \
 	-nographic \
-	-M mega2560 \
-	-bios ./firmware/qemu-avr-tests/free-rtos/Demo/AVR_ATMega2560_GCC/demo.elf \
-	-s \
+	-M lc3-machine \
+	-cpu lc3-cpu \
+	-bios ./2048.obj \
 	-D log.txt \
 	-monitor pipe:mon \
-	-serial mon:stdio \
 	-d in_asm,op,out_asm \
+	#-serial mon:stdio \
+	#-bios ./firmware/qemu-avr-tests/free-rtos/Demo/AVR_ATMega2560_GCC/demo.elf \
 	#-plugin ../../qemu-8.1.0/build/contrib/plugins/libexeclog.so \
 	#-d plugin \
 	#-serial tcp::5678,server=on,wait=off \
