348a249ab1b8f536aba4eae3dc59f6f45625fbcf
chumoath <chumoath.guo@gmail.com>
2024-06-26 21:10:04 +0800

    提交lc3的运行脚本

--- avr/start_boot.sh
+++ avr/start_boot.sh
@@ -1,17 +1,20 @@
 #!bin/sh
 
-
+# 重定向monitors之后，可以通过 ctrl + c 退出
 #gdb -args ../../qemu-8.1.0/build/qemu-system-avr \
 #../../qemu-8.1.0/build/qemu-system-avr \
-#../../../../qemu-lc3/qemu-8.1.0/build/qemu-system-lc3 \
-gdb -args ../../../../qemu-lc3/qemu-8.1.0/build/qemu-system-lc3 \
+#gdb -args ../../../../qemu-lc3/qemu-8.1.0/build/qemu-system-lc3 \
+../../../../qemu-lc3/qemu-8.1.0/build/qemu-system-lc3 \
 	-nographic \
 	-M lc3-machine \
 	-cpu lc3-cpu \
 	-bios ./2048.obj \
 	-D log.txt \
-	-monitor pipe:mon \
 	-d in_asm,op,out_asm \
+	-monitor telnet::44444,server,nowait,nodelay
+	#-monitor pipe:mon \
+
+	# -d in_asm
 	#-serial mon:stdio \
 	#-bios ./firmware/qemu-avr-tests/free-rtos/Demo/AVR_ATMega2560_GCC/demo.elf \
 	#-plugin ../../qemu-8.1.0/build/contrib/plugins/libexeclog.so \
